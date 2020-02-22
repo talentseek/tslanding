5996c3b0461510a9e63a255f9215ed81f73f6ace
talentseek <talentseekio@gmail.com>
2020-02-22 17:34:40 +0000

    initial how page

--- app.rb
+++ app.rb
@@ -4,4 +4,20 @@ get '/' do
 erb :index, layout: :homelayout
 end
 
+get '/how' do
+erb :how, layout: :genlayout
+end
+
+get '/access' do
+erb :access, layout: :genlayout
+end
+
+get '/thankyou' do
+erb :thankyou, layout: :genlayout
+end
+
+
+
+
+
 
